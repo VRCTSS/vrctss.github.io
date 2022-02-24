bb1142208eb618100dbda1c0aec620b425aa6e29
vrctss <gumi+vrctss@jo3qma.com>
2022-02-24 20:56:23 +0900

    1frame

--- mk_poster.sh
+++ mk_poster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/ffmpeg -r 1 -i poster_blank.png -s 2816x3976 -pix_fmt yuv420p -r 10  poster_test.mp4
+/usr/bin/ffmpeg -r 1 -i poster_blank.png -s 2816x3976 -pix_fmt yuv420p -r 1  poster_test.mp4
